9004e4313df8649636d2e006bee1ad3f5c7e0d45
Brian Greenacre <bgreenacre42@gmail.com>
2012-05-02 14:34:03 -0500

    Added an post-install task.

---
+++ src/Wordpress/Composer/Tasks.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wordpress\Composer;
+
+use Composer\Script\Event;
+
+class Tasks {
+
+    public static function init(Event $event)
+    {
+        $composer = $event->getComposer();
+
+        var_dump($composer);
+    }
+
+}
