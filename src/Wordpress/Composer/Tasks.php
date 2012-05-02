824dd7ef912d9ff978266d129615e6537c61f40f
Brian Greenacre <bgreenacre42@gmail.com>
2012-05-02 15:39:34 -0500

    Getting symlinks setup.

--- src/Wordpress/Composer/Tasks.php
+++ src/Wordpress/Composer/Tasks.php
@@ -8,9 +8,64 @@ class Tasks {
 
     public static function init(Event $event)
     {
-        $root = __DIR__ . '/../../../../../../';
+        $root = realpath(__DIR__ . '/../../../../../../');
 
-        var_dump(realpath($root));
+        if ($root === FALSE)
+        {
+            $root .= DIRECTORY_SEPARATOR;
+        }
+
+        $paths = array(
+            'public/wordpress/wp-content/themes',
+            'public/wordpress/wp-content/plugins',
+        );
+
+        foreach ($paths as $path)
+        {
+            $target = substr($path, strrpos($path, '/'));
+
+            if (is_dir($root . $path))
+            {
+                self::rrmdir($root . $path);
+            }
+
+            if ( ! is_dir($root . $target))
+            {
+                mkdir($root . $target, 0755);
+                chmod($root . $target, 0755);
+            }
+
+            symlink($root . $path, $root . $target);
+        }
+    }
+
+    public static function rrmdir($dir)
+    {
+        $fp = opendir($dir);
+
+        if ($fp)
+        {
+            while ($f = readdir($fp))
+            {
+                $file = $dir . DIRECTORY_SEPARATOR . $f;
+
+                if ($f == '.' OR $f == '..')
+                {
+                    continue;
+                }
+                elseif (is_dir($file) AND ! is_link($file))
+                {
+                    self::rrmdir($file);
+                }
+                else
+                {
+                    unlink($file);
+                }
+            }
+
+            closedir($fp);
+            rmdir($fp);
+        }
     }
 
 }
